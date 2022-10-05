31654f9ab0e8d4855152d3a9e498bba4e1f2b437
Retrowave-glitch <72885208+Retrowave-glitch@users.noreply.github.com>
2022-10-04 23:01:52 -0400

    Camera follow code
    don't know if this is 100% needed, but I made it just incase.

---
+++ Assets/code folder/SmoothCameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float damping;
+
+    private Vector3 velocity = Vector3.zero;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movePosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+    }
+}

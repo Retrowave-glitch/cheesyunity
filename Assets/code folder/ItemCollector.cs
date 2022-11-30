67aa170ff6ef1d003bf25bef5b2610666c406fe1
Retrowave-glitch <72885208+Retrowave-glitch@users.noreply.github.com>
2022-11-29 22:36:41 -0500

    Bug fix
    Finally found out the bug that caused the game to crash when player 2 collected the cheese as well, so now both players could interact with the objects!

--- Assets/code folder/ItemCollector.cs
+++ Assets/code folder/ItemCollector.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-public class ItemCollector : MonoBehaviour
+public class ItemCollector : NetworkBehaviour
 {
     private int Cheese = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Cheese"))
+        if (collision.gameObject.CompareTag("Cheese") && (IsServer))
         {
             collision.GetComponent<NetworkObject>().Despawn(true);
             Destroy(collision.gameObject);
